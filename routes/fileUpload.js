b031a447794f93d3a720e29c42f776f90198b298
Avinash Kumar <avinash.kumar@walmartlabs.com>
2021-07-16 23:22:51 +0530

    Changes for the file upload fix

--- routes/fileUpload.js
+++ routes/fileUpload.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const FileController = require("../controllers/FileUploadController");
+const multer = require("multer");
 
 const router = express.Router();
 
-router.post("/", FileController.FileUploads);
+router.post("/", multer().single('file'), FileController.FileUploads);
 
 module.exports = router;
