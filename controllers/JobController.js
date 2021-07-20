61f8914c5b6992470a91c5ca9582cf2b5bdc7f7d
raj tejani <rajtejani9@gmail.com>
2021-07-20 20:44:30 +0530

    incremental commit

--- controllers/JobController.js
+++ controllers/JobController.js
@@ -1,7 +1,10 @@
 const JobModel = require("../models/JobModel");
+const UserModel = require("../models/UserModel");
 const { body, validationResult } = require("express-validator");
 const { constants } = require("../helpers/constants");
-const { sanitizeBody } = require("express-validator");
+const moment = require("moment");
+const { sanitizeBody, sanitize } = require("express-validator");
+const mailer = require("../helpers/mailer");
 const apiResponse = require("../helpers/apiResponse");
 const auth = require("../middlewares/jwt");
 var mongoose = require("mongoose");
@@ -162,5 +165,260 @@ exports.addJob = [
       //throw error in json response with status 500.
       return apiResponse.ErrorResponse(res, err);
     }
+  },
+];
+
+// This function sends interview scheduling mail to cadidate
+exports.scheduleInterview = [
+  auth,
+  body("jobId", "jobId must not be empty.").notEmpty(),
+  body("candidateId", "candidateId must not be empty.").notEmpty(),
+  body("interviewDate", "interviewDate must not be empty.").notEmpty(),
+  body("interviewTime", "interviewTime must not be empty.").notEmpty(),
+  body("message", "message must not be empty.").notEmpty(),
+  sanitize("*"),
+  (req, res) => {
+    const user = req.user;
+
+    if (user.userType !== constants.userTypes.RECRUITER)
+      return apiResponse.unauthorizedResponse(res, "Invalid Permission.");
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return apiResponse.validationErrorWithData(
+        res,
+        "Validation Error.",
+        errors.array()
+      );
+    }
+
+    const { jobId, candidateId, interviewDate, interviewTime, message } =
+      req.body;
+
+    JobModel.findOne({ _id: jobId }, (error, job) => {
+      if (error) {
+        return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+      }
+
+      if (!job) {
+        return apiResponse.ErrorResponse(res, "Job not found.");
+      }
+
+      UserModel.findOne({ _id: candidateId }, (error, user) => {
+        if (error) {
+          return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+        }
+
+        if (!user) {
+          return apiResponse.ErrorResponse(res, "Candidate doesn't exists.");
+        }
+
+        JobModel.updateOne(
+          { _id: jobId, "applicants.candidate": candidateId },
+          {
+            $set: {
+              "applicants.$.status": constants.applicationStatus.INTERVIEWING,
+            },
+          },
+          {
+            new: true,
+          },
+          (error, result) => {
+            if (error) {
+              return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+            }
+
+            mailer
+              .send(
+                constants.confirmEmails.from,
+                user.email,
+                `Scheduled Interview on ${moment(interviewDate).format(
+                  "DD/mm/yyyy"
+                )} at ${moment(interviewTime).format("hh:mm")}`,
+                message
+              )
+              .then(() => {
+                return apiResponse.successResponse(res, "Interview scheduled.");
+              })
+              .catch((error) => {
+                if (error) {
+                  return apiResponse.ErrorResponse(
+                    res,
+                    "Operation Failed.",
+                    error
+                  );
+                }
+              });
+          }
+        );
+      });
+    });
+  },
+];
+
+// This function sends offer letter to candidtes
+exports.sendOfferLetter = [
+  auth,
+  body("jobId", "jobId must not be empty.").notEmpty(),
+  body("candidateId", "candidateId must not be empty.").notEmpty(),
+  body("message", "message must not be empty.").notEmpty(),
+  sanitize("*"),
+  (req, res) => {
+    const user = req.user;
+
+    if (user.userType !== constants.userTypes.RECRUITER)
+      return apiResponse.unauthorizedResponse(res, "Invalid Permission.");
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return apiResponse.validationErrorWithData(
+        res,
+        "Validation Error.",
+        errors.array()
+      );
+    }
+
+    const { jobId, candidateId, message } = req.body;
+
+    JobModel.findOne({ _id: jobId }, (error, job) => {
+      if (error) {
+        return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+      }
+
+      if (!job) {
+        return apiResponse.ErrorResponse(res, "Job not found.");
+      }
+
+      UserModel.findOne({ _id: candidateId }, (error, user) => {
+        if (error) {
+          return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+        }
+
+        if (!user) {
+          return apiResponse.ErrorResponse(res, "Candidate doesn't exists.");
+        }
+
+        JobModel.updateOne(
+          { _id: jobId, "applicants.candidate": candidateId },
+          {
+            $set: {
+              "applicants.$.status": constants.applicationStatus.HIRING,
+            },
+          },
+          {
+            new: true,
+          },
+          (error, result) => {
+            if (error) {
+              return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+            }
+
+            mailer
+              .send(
+                constants.confirmEmails.from,
+                user.email,
+                `Offer Letter`,
+                message
+              )
+              .then(() => {
+                return apiResponse.successResponse(res, "Offer Letter Sent.");
+              })
+              .catch((error) => {
+                if (error) {
+                  return apiResponse.ErrorResponse(
+                    res,
+                    "Operation Failed.",
+                    error
+                  );
+                }
+              });
+          }
+        );
+      });
+    });
+  },
+];
+
+// This function sends regret letter
+exports.sendRegretLetter = [
+  auth,
+  body("jobId", "jobId must not be empty.").notEmpty(),
+  body("candidateId", "candidateId must not be empty.").notEmpty(),
+  body("message", "message must not be empty.").notEmpty(),
+  sanitize("*"),
+  (req, res) => {
+    const user = req.user;
+
+    if (user.userType !== constants.userTypes.RECRUITER)
+      return apiResponse.unauthorizedResponse(res, "Invalid Permission.");
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return apiResponse.validationErrorWithData(
+        res,
+        "Validation Error.",
+        errors.array()
+      );
+    }
+
+    const { jobId, candidateId, interviewDate, interviewTime, message } =
+      req.body;
+
+    JobModel.findOne({ _id: jobId }, (error, job) => {
+      if (error) {
+        return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+      }
+
+      if (!job) {
+        return apiResponse.ErrorResponse(res, "Job not found.");
+      }
+
+      UserModel.findOne({ _id: candidateId }, (error, user) => {
+        if (error) {
+          return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+        }
+
+        if (!user) {
+          return apiResponse.ErrorResponse(res, "Candidate doesn't exists.");
+        }
+
+        JobModel.updateOne(
+          { _id: jobId, "applicants.candidate": candidateId },
+          {
+            $set: {
+              "applicants.$.status": constants.applicationStatus.REJECTED,
+            },
+          },
+          {
+            new: true,
+          },
+          (error, result) => {
+            if (error) {
+              return apiResponse.ErrorResponse(res, "Operation Failed.", error);
+            }
+
+            mailer
+              .send(
+                constants.confirmEmails.from,
+                user.email,
+                `Regret Letter`,
+                message
+              )
+              .then(() => {
+                return apiResponse.successResponse(res, "Regret Letter Sent.");
+              })
+              .catch((error) => {
+                if (error) {
+                  return apiResponse.ErrorResponse(
+                    res,
+                    "Operation Failed.",
+                    error
+                  );
+                }
+              });
+          }
+        );
+      });
+    });
   },
 ];
--- routes/job.js
+++ routes/job.js
@@ -6,5 +6,8 @@ var router = express.Router();
 router.get("/", JobController.JobList);
 router.post("/", JobController.addJob);
 router.get("/jobById", JobController.JobById);
+router.post("/scheduleInterview", JobController.scheduleInterview);
+router.post("/sendOfferLetter", JobController.sendOfferLetter);
+router.post("/sendRegretLetter", JobController.sendRegretLetter);
 
 module.exports = router;
